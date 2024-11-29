803fd7fe935470d5a562bd232434336acf83a11c
alxis-code <adriansanchezlara@gmail.com>
2024-11-30 00:23:17 +0100

    display the number of words in a book

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ def get_book_text_by_path(path):
     with open(path) as f:
         return f.read()
 
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+
 if __name__ == "__main__":
     text = get_book_text_by_path("books/frankenstein.txt")
-    print(text)
+    print(count_words(text))
