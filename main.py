5049e12dfcdb4db329b88ea9634988623c848613
alxis-code <adriansanchezlara@gmail.com>
2024-11-30 00:38:04 +0100

    count every character function

--- main.py
+++ main.py
@@ -8,6 +8,19 @@ def count_words(text):
     return len(words)
 
 
+def count_characters_insensitive(text):
+    text = text.lower()
+    characters = dict()
+    for char in text:
+        if char not in characters:
+            characters[char] = 1
+        characters[char] += 1
+    print(characters)
+
+
 if __name__ == "__main__":
     text = get_book_text_by_path("books/frankenstein.txt")
+    print("Number of words:")
     print(count_words(text))
+    print("Counted characters:")
+    print(count_characters_insensitive(text))
